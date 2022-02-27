c12eac464abffbcb0f012847e7dc6b6f75c1e587
designer233 <84735836+designer233@users.noreply.github.com>
2022-02-27 11:45:35 +0900

    Create plus_minus.py

---
+++ Hackerrank/plus_minus.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    N = len(arr)
+    
+    r_diag = 0
+    l_diag = 0
+    for x in range(N):
+        r_diag += arr[x][x]
+        l_diag += arr[x][-(x+1)]
+    
+    #print("r, l", r_diag, l_diag)
+    return abs(r_diag - l_diag)
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+diagonal.py
+    plusMinus(arr)
