ffe7fa6c2e5c9296e4e11d856881d076dd7a4560
designer233 <84735836+designer233@users.noreply.github.com>
2022-02-27 11:14:03 +0900

    Delete capitalize.py

--- capitalize.py
+++
@@ -1,9 +0,0 @@
-# Complete the solve function below.
-def solve(s):
-    lst = s.split(" ")
-
-    res = []
-    for item in lst:
-        res.append(item.capitalize())
-        
-    return " ".join(res)
