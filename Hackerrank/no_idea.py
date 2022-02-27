df1bfe2bddb6b3954d91f8c4b4fdff7e8ed63001
designer233 <84735836+designer233@users.noreply.github.com>
2022-02-27 11:19:05 +0900

    Create no_idea.py

---
+++ Hackerrank/no_idea.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n, m = list(map(int, input().split()))
+
+arr = list(map(int, input().split()))
+
+A = set(map(int, input().split()))
+
+B = set(map(int, input().split()))
+
+happiness = 0
+
+for item in arr:
+    if item in A:
+        happiness += 1
+    elif item in B:
+        happiness -= 1
+
+print(happiness)
